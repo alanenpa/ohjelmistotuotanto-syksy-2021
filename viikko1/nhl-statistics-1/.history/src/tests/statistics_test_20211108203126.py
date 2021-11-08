0cb680de7403bb916c5e38fcca171b6de9c670cc
Patrik Alanen <alanen.patrik@gmail.com>
2021-11-08 20:59:29 +0200

    Tehtävät 14-16

---
+++ viikko1/nhl-statistics-1/.history/src/tests/statistics_test_20211108203126.py
@@ -0,0 +1,39 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search_works(self):
+        result = self.statistics.search("Kurri")
+
+        self.assertAlmostEqual(result.name, "Kurri")
+        self.assertAlmostEqual(result.team, "EDM")
+        self.assertAlmostEqual(result.goals, 37)
+        self.assertAlmostEqual(result.assists, 53)
+
+    def test_search_returns_none(self):
+        result = self.statistics.search("Xyz")
+
+        self.assertIsNone(result)
+
+    def test_correct_players_with_team_method(self):
+        result = self.statistics.team("EDM")
+
+        self.asser
+        print(result[0], result[1], result[2])
---
+++ viikko1/nhl-statistics-1/.history/src/tests/statistics_test_20211108204014.py
@@ -0,0 +1,46 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search_works(self):
+        result = self.statistics.search("Kurri")
+
+        self.assertAlmostEqual(result.name, "Kurri")
+        self.assertAlmostEqual(result.team, "EDM")
+        self.assertEqual(result.goals, 37)
+        self.assertEqual(result.assists, 53)
+
+    def test_search_returns_none(self):
+        result = self.statistics.search("Xyz")
+
+        self.assertIsNone(result)
+
+    def test_correct_players_with_team_method(self):
+        playerlist = self.statistics.team("EDM")
+
+        self.assertAlmostEqual(playerlist[0].name, "Semenko")
+        self.assertEqual(len(playerlist), 3)
+
+    def test_top_scorer(self):
+        result = self.statistics.top_scorers(4)
+        print(result[0].name)
+        print(result[1].name)
+        print(result[0].name)
+        self.assertEqual(result[0].name, "Gretzky")
